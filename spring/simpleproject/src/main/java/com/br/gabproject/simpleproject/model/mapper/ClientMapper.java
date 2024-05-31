7e2cdc3568dea160d59336bb5c4d9366db2438cb
Gabr1el94 <bielsystem64@gmail.com>
2024-05-31 17:11:58 -0300

    add role on mapper

--- spring/simpleproject/src/main/java/com/br/gabproject/simpleproject/model/mapper/ClientMapper.java
+++ spring/simpleproject/src/main/java/com/br/gabproject/simpleproject/model/mapper/ClientMapper.java
@@ -15,6 +15,7 @@ public class ClientMapper implements RowMapper<Client> {
         client.setIdPessoa(rs.getInt("idpessoa"));
         client.setName(rs.getString("name"));
         client.setEmail(rs.getString("email"));
+        client.setRole(rs.getString("role"));
         return client;
     }
 
